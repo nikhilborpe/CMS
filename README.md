6a83e2d0e1fc41c6c1d510cc6a54b8bdb606f779
nikhilborpe <nikhil.borpe3@gmail.com>
2016-06-08 16:58:59 +0530

    Boiler plate for a CMS application

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# CMS
-This is the application for managing the compliances in an organization
+# cms
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cmsApp
+ * @description
+ * # cmsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cmsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
